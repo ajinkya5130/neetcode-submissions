a8d36778ec965f984e6ae602adb14b3525d5f9c9
Ajinkya Kalaskar <ajinkya5130@gmail.com>
2026-09-08 21:26:30 +0530

    Add: top-k-elements-in-list - submission-3

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-3.kt
@@ -0,0 +1,24 @@
+class Solution {
+    fun topKFrequent(nums: IntArray, k: Int): IntArray {
+        val count = HashMap<Int, Int>()
+        val freq = List(nums.size + 1) { mutableListOf<Int>() }
+
+        for (num in nums) {
+            count[num] = count.getOrDefault(num, 0) + 1
+        }
+        for ((num, cnt) in count) {
+            freq[cnt].add(num)
+        }
+
+        val res = mutableListOf<Int>()
+        for (i in freq.size - 1 downTo 1) {
+            for (num in freq[i]) {
+                res.add(num)
+                if (res.size == k) {
+                    return res.toIntArray()
+                }
+            }
+        }
+        return res.toIntArray()
+    }
+}
